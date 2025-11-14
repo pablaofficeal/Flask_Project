8d2326729f7a81571d406e483e5237f75cc16fd5
pablaofficeal <pavlogorosko56@gmail.com>
2025-11-14 23:29:36 +0100

    Добавлен функционал для логина пользователя и создан шаблон страницы входа

---
+++ backend/routers/check/oauth/login.py
@@ -0,0 +1,30 @@
+from flask import (
+    Blueprint, 
+    request, 
+    jsonify, 
+    session, 
+    redirect, 
+    url_for, 
+    render_template
+)
+from models.main_rou_imp_db import User
+
+login_bpp = Blueprint('login_bpp', __name__)
+
+@login_bpp.route('/login', methods=['GET', 'POST'])
+def login():
+    """ Добавляем логику для логина юзера """
+    if request.method == 'POST':
+        """ Добавляем логику для логина юзера """
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['user_id'] = user.id
+            session['username'] = username
+            session['email'] = user.email
+            return redirect(url_for('homes_bpp.home'))
+        else:
+            return jsonify({"message": "Invalid credentials"}), 401
+    return render_template('login.html')
