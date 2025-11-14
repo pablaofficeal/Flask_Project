ce45546f441ab8f9e137d52804e3cfa644a98718
pablaofficeal <pavlogorosko56@gmail.com>
2025-11-14 23:33:43 +0100

    Добавлен функционал для страницы входа: зарегистрированы маршруты и добавлен CSS для оформления

--- backend/import_all_bp.py
+++ backend/import_all_bp.py
@@ -1,8 +1,10 @@
 from import_router_bp import (
     home_bpp,
-    register_bpp
+    register_bpp,
+    login_bpp
 )
 
 def register_blueprints(app):
     app.register_blueprint(home_bpp)
     app.register_blueprint(register_bpp)
+    app.register_blueprint(login_bpp)
--- backend/import_router_bp.py
+++ backend/import_router_bp.py
@@ -1,2 +1,3 @@
 from routers.home.main_home import home_bpp
 from routers.check.oauth.register import register_bpp
+from routers.check.oauth.login import login_bpp
