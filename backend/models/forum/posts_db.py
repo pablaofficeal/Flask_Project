53926b7efe21dc8480fc0c910fc86b6f6d99c1e8
pablaofficeal <pavlogorosko56@gmail.com>
2025-11-21 20:41:37 +0100

    Пофиксил ошибку с github oauth2 за 40 минут на стриме поправил маршруты и сделал fix BD

--- backend/models/forum/posts_db.py
+++ backend/models/forum/posts_db.py
@@ -1,13 +1,17 @@
 from models.imp import db
+from sqlalchemy.orm import relationship
 
 class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
+
+    # Связь с пользователем - убрал backref чтобы избежать конфликтов
+    user = relationship('User')
 
     def __repr__(self):
         return f"<Post id={self.id} user_id={self.user_id} title={self.title}>"
--- backend/routers/home/home.py
+++ backend/routers/home/home.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
-from models.main_rou_imp_db import User
+from models.main_rou_imp_db import User, Post
 from models.imp import db
 
 homes_bpp = Blueprint('homes_bpp', __name__)
 
 @homes_bpp.route('/home')
 def home():
-    
+    # Получаем все посты из базы данных
+    posts = Post.query.all()
+    # Получаем все пользователи из базы данных
     users = User.query.all()
-    return render_template('home.html', users=users)
+    return render_template('home.html', users=users, posts=posts)
