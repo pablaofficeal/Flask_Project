b4808a3785cee115709e3f8fdbf3742f40a46fed
pablaofficeal <pavlogorosko56@gmail.com>
2025-11-21 13:16:10 +0100

    Пофиксил ошибку с github oauth2 за 40 минут на стриме поправил маршруты и сделал fix BD

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,6 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
-
     IP = os.getenv("IP")
     PORT = os.getenv("PORT")
     DEBUG = "False"
